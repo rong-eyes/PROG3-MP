408c2ce18e6a383f747a684dda05ed640c0f6ed9
Rachelle9226 <paredes.rachelle02@gmail.com>
2026-07-10 00:23:31 +0800

    Update InventoryItem.java

--- InventoryItem.java
+++ InventoryItem.java
@@ -1,12 +1,33 @@
 package MCO1;
 
+
 public class InventoryItem {
+
+	public static final String TYPE_INGREDIENT = "INGREDIENT";
+	public static final String TYPE_BASE = "BASE";
+
+	private String type;
 	private String name;
+	private int price;
 	private int quantity;
 
-	public InventoryItem(String name, int quantity) {
+	public InventoryItem(String type, String name, int price, int quantity) {
+		this.type = type;
 		this.name = name;
+		this.price = price;
 		this.quantity = quantity;
+	}
+
+	public InventoryItem(String type, String name, int quantity) {
+		this(type, name, Market.sellPriceOf(name), quantity);
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
 	}
 
 	public String getName() {
@@ -15,6 +36,14 @@ public class InventoryItem {
 
 	public void setName(String name) {
 		this.name = name;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
 	}
 
 	public int getQuantity() {

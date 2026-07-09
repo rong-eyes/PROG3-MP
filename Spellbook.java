53af1dbfebb1520ded31afc3557446a7a389ef89
Rachelle9226 <paredes.rachelle02@gmail.com>
2026-07-10 00:20:29 +0800

    Update Spellbook.java

--- Spellbook.java
+++ Spellbook.java
@@ -3,9 +3,9 @@ import java.util.ArrayList;
 
 public class Spellbook {
 	private ArrayList<Recipe> unlockedRecipes;
-	
+
 	public Spellbook() {
-		//add default recipes
+		unlockedRecipes = new ArrayList<>();
 	}
 
 	public ArrayList<Recipe> getUnlockedRecipes() {
@@ -15,49 +15,61 @@ public class Spellbook {
 	public void setUnlockedRecipes(ArrayList<Recipe> unlockedRecipes) {
 		this.unlockedRecipes = unlockedRecipes;
 	}
-	
-	public void sortRecipes() { //insertion sort
-								//must be called right after a recipe is unlocked
-		   int i, j;
-		    for (i = 1; i < unlockedRecipes.size(); i++) {
-		        Recipe tmp = unlockedRecipes.get(i);
-		        j = i;
-		        while ((j > 0) && (unlockedRecipes.get(j - 1).getConcoctionID() > tmp.getConcoctionID())) {
-		        	unlockedRecipes.set(j, unlockedRecipes.get(j - 1));
-		            j--;
-		        }
-		        unlockedRecipes.set(j, tmp);
-		    }
+
+	public void sortRecipes() {
+		int i, j;
+		for (i = 1; i < unlockedRecipes.size(); i++) {
+			Recipe tmp = unlockedRecipes.get(i);
+			j = i;
+			while ((j > 0) && (unlockedRecipes.get(j - 1).getConcoctionID() > tmp.getConcoctionID())) {
+				unlockedRecipes.set(j, unlockedRecipes.get(j - 1));
+				j--;
+			}
+			unlockedRecipes.set(j, tmp);
+		}
 	}
-	
-	public Recipe getRecipe(int id) { //for recipe mode
+
+	public Recipe getRecipe(int id) {
 		int lo = 0;
 		int hi = unlockedRecipes.size() - 1;
-		
-		while(lo <= hi) {
+
+		while (lo <= hi) {
 			int mid = lo + (hi - lo) / 2;
-			
-			if(id == unlockedRecipes.get(mid).getConcoctionID()) {
+
+			if (id == unlockedRecipes.get(mid).getConcoctionID()) {
 				return unlockedRecipes.get(mid);
-			}else if(id > unlockedRecipes.get(mid).getConcoctionID()) {
+			} else if (id > unlockedRecipes.get(mid).getConcoctionID()) {
 				lo = mid + 1;
-			}else
-				hi = mid -1;
-		}
-		
-		return null;  //recipe not found
-	}
-	
-	public void printRecipes() { //might need to be modified once GUI is involved
-		for(int i = 0; i < unlockedRecipes.size(); i++) {
-			System.out.println(unlockedRecipes.get(i).getConcoctionID() + unlockedRecipes.get(i).getName() + "\t" + unlockedRecipes.get(i).getPrice());
-			System.out.println("\t" + unlockedRecipes.get(i).getConcoctionBase() + "\n");
-			for(int j = 0; j < unlockedRecipes.get(i).getIngredients().size(); j++) {
-				System.out.print("\t" + unlockedRecipes.get(i).getIngredients().get(j).getName() + " ");
+			} else {
+				hi = mid - 1;
 			}
 		}
+
+		return null; // recipe not found
 	}
-	
+
+	public void printRecipes() {
+		System.out.println("========== SPELLBOOK ==========");
+		if (unlockedRecipes.isEmpty()) {
+			System.out.println("Your spellbook is empty.");
+			return;
+		}
+		for (int i = 0; i < unlockedRecipes.size(); i++) {
+			Recipe recipe = unlockedRecipes.get(i);
+			System.out.printf("[%03d] %s (%d crystals)%n",
+					recipe.getConcoctionID(), recipe.getName(), recipe.getPrice());
+			System.out.println("      Base: " + recipe.getConcoctionBase().getName());
+			StringBuilder ingredients = new StringBuilder();
+			for (int j = 0; j < recipe.getIngredients().size(); j++) {
+				if (j > 0) {
+					ingredients.append(", ");
+				}
+				ingredients.append(recipe.getIngredients().get(j).getName());
+			}
+			System.out.println("      Ingredients: " + ingredients);
+		}
+	}
+
 	public void addRecipe(Recipe recipe) {
 		unlockedRecipes.add(recipe);
 		sortRecipes();

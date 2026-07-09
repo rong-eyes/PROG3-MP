99a22e9a366377c232f7723b76807bb1b3d303ee
Rachelle9226 <paredes.rachelle02@gmail.com>
2026-07-09 22:38:40 +0800

    Reformat InventoryItem.java code style

--- InventoryItem.java
+++ InventoryItem.java
@@ -3,15 +3,16 @@ package MCO1;
 public class InventoryItem {
 	private String name;
 	private int quantity;
-	
+
 	public InventoryItem(String name, int quantity) {
-		super();
 		this.name = name;
 		this.quantity = quantity;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
@@ -19,17 +20,16 @@ public class InventoryItem {
 	public int getQuantity() {
 		return quantity;
 	}
-	
+
 	public void setQuantity(int qty) {
 		quantity = qty;
 	}
-	
-	public void deductQuantity(int qty) { //for selling or using in a potion
+
+	public void deductQuantity(int qty) {
 		this.quantity -= qty;
 	}
-	
+
 	public void addQuantity(int qty) {
 		this.quantity += qty;
 	}
-	
 }

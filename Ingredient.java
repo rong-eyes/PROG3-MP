72f4702bcd7c2eb8375179375d9f09566b3a2635
Rachelle9226 <paredes.rachelle02@gmail.com>
2026-07-09 22:30:22 +0800

    Update Ingredient.java

--- Ingredient.java
+++ Ingredient.java
@@ -1,7 +1,33 @@
 package MCO1;
 
-public class Ingredient extends InventoryItem {
+public class Ingredient {
+	private InventoryItem item;
+
 	public Ingredient(String name, int quantity) {
-		super(name, quantity);
+		item = new InventoryItem(name, quantity);
+	}
+
+	public String getName() {
+		return item.getName();
+	}
+
+	public void setName(String name) {
+		item.setName(name);
+	}
+
+	public int getQuantity() {
+		return item.getQuantity();
+	}
+
+	public void setQuantity(int qty) {
+		item.setQuantity(qty);
+	}
+
+	public void deductQuantity(int qty) {
+		item.deductQuantity(qty);
+	}
+
+	public void addQuantity(int qty) {
+		item.addQuantity(qty);
 	}
 }

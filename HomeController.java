43109533e51ec19ce7d404e436713155bd5df072
Yre <yreana_yu@dlsu.edu.ph>
2026-07-29 17:52:20 +0800

    added welcome messages

--- HomeController.java
+++ HomeController.java
@@ -6,12 +6,15 @@ public class HomeController {
 	private HomeModel model;
 	private PotionProdigy main;
 	private Player currentPlayer;
+	private boolean newGame;
 	
-	public HomeController(HomePanel view, HomeModel model, PotionProdigy main, Player p) {
+	public HomeController(HomePanel view, HomeModel model, PotionProdigy main, Player p, boolean newGame) {
 		this.view = view;
-		this.model = model;
+		this.model= model;
 		this.main = main;
 		this.currentPlayer = p;
+		this.newGame = newGame;
+		
 		
 		view.cabinetListener(new MouseAdapter() {
 			@Override
@@ -47,6 +50,17 @@ public class HomeController {
 				collectLogin();
 			}
 		});
+		
+		view.arrowListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				back();
+			}
+		});
+		
+		while(this.promptWelcome() == 0) {
+			
+		}
 	}
 	
 	public void checkInventory() {
@@ -58,7 +72,18 @@ public class HomeController {
 	}
 	
 	public void cauldronClick() {
-		//insert code
+		int scenario = view.promptBrewOrBless();
+		
+		if(scenario == 1) { //Brew Concoction
+			boolean isRecipe = view.promptBrewMode();
+			if(isRecipe) {
+				System.out.println("Recipe");
+			}else
+				System.out.println("Creative");
+			
+		}else if(scenario == 2) { //Bless Cauldron
+			model.checkBrokenCauldrons(currentPlayer);
+		}
 	}
 	
 	public void checkSpellbook() {
@@ -69,7 +94,15 @@ public class HomeController {
 		//insert code
 	}
 
+	public void back() {
+		//saves the game when pressing arrow
+		SaveManager.saveGame(getCurrentPlayer());
+		this.main.TitleScreen();
+	}
 	
+	public int promptWelcome() {
+		return view.WelcomeMessage(this.currentPlayer.getPlayerName(), this.newGame);
+	}
 	
 	//PUT OTHER CODES ABOVE GETTER SETTERS
 	public HomePanel getView() {
@@ -78,14 +111,6 @@ public class HomeController {
 
 	public void setView(HomePanel view) {
 		this.view = view;
-	}
-
-	public HomeModel getModel() {
-		return model;
-	}
-
-	public void setModel(HomeModel model) {
-		this.model = model;
 	}
 
 	public PotionProdigy getMain() {
@@ -102,6 +127,14 @@ public class HomeController {
 
 	public void setCurrentPlayer(Player currentPlayer) {
 		this.currentPlayer = currentPlayer;
+	}
+
+	public HomeModel getModel() {
+		return model;
+	}
+
+	public void setModel(HomeModel model) {
+		this.model = model;
 	}
 	
 }

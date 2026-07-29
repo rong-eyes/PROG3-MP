cfa1b8fb6f972a8717f960006c136cbec2489ab9
Yre <yreana_yu@dlsu.edu.ph>
2026-07-29 17:52:57 +0800

    added checkBrokenCauldron() skelelton

--- HomeModel.java
+++ HomeModel.java
@@ -1,5 +1,8 @@
-import java.util.ArrayList;
-
 public class HomeModel { //Model for the Home Screen; previous 'Main.java'
 
+	public boolean checkBrokenCauldrons(Player p) {
+		//insert code to check
+		return false;
+	}
+	
 }

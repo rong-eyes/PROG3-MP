bf08380e6a9474ccb19e5a542334d4ab5819600d
Rachelle9226 <paredes.rachelle02@gmail.com>
2026-08-02 00:36:18 +0800

    Update SpellbookController.java

--- SpellbookController.java
+++ SpellbookController.java
@@ -1,79 +1,119 @@
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
-public class SpellbookController implements RecipeListener{
+public class SpellbookController {
 	private SpellbookModel model;
 	private SpellbookPanel view;
 	private PotionProdigy main;
 	private Player currentPlayer;
-	
+
 	public SpellbookController(SpellbookModel m, SpellbookPanel v, PotionProdigy main, Player p) {
 		this.model = m;
 		this.view = v;
 		this.main = main;
 		this.currentPlayer = p;
-			model.setUnlockedRecipes(currentPlayer.getSpellbook().getUnlockedRecipes());
-	
+
+		model.loadFrom(p);
+
+		for(int i = 0; i < view.getRowsPerPage(); i++) {
+			final int row = i;
+			view.rowListener(i, new MouseAdapter() {
+				@Override
+				public void mouseClicked(MouseEvent e) {
+					openRecipe(row);
+				}
+			});
+		}
+
+		view.leftArrowListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				previousPage();
+			}
+		});
+
+		view.rightArrowListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				nextPage();
+			}
+		});
+
+		view.searchListener(e -> runSearch());
+
 		view.arrowListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				back();
 			}
 		});
-		
-		view.setRecipeListener(this); //works because it implements recipe listenter
 
-		view.leftArrListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(model.prevPage(view.getSb().getUnlockedRecipes().size(), view.getCurrentPage())) {
-					view.setCurrentPage(view.getCurrentPage() - 1);
-					view.displayPage();
-				}
-						
-			}
-		});
-		
-		view.rightArrListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent e) {
-				if(model.nextPage(view.getSb().getUnlockedRecipes().size(), view.getCurrentPage(), view.getRECIPES_PER_PAGE())) {
-					view.setCurrentPage(view.getCurrentPage() + 1);
-					view.displayPage();
-				}
-						
-			}
-		});
-		
-		
-		final int[] result = new int[1];
-		view.textListener(e -> {
-			try {
-				result[0] = Integer.parseInt(view.getSearchBar().getText().trim());
-				view.getSearchBar().setText("");
-				
-				Recipe temp = model.getRecipe(result[0]);
-				if(temp != null) {
-					view.RecipeDisplay(temp);
-				}
-					
-			} catch (NumberFormatException ex) {
-				view.getSearchBar().setText("");
-			}
-			
-		});
-	}
-	
-	@Override
-	public void RecipeClicked(Recipe r) {
-		view.RecipeDisplay(r);
+		view.setDetail(model.recipeDetail(null));
+		writePage();
 	}
 
+	/**
+	* Displays the current page of recipes on the left page and clears any unused lines.
+	*/
+	public void writePage() {
+		ArrayList<Recipe> page = model.recipesOnPage();
+
+		for(int i = 0; i < view.getRowsPerPage(); i++) {
+			if(i < page.size())
+				view.setRow(i, model.recipeLine(page.get(i)), true);
+			else
+				view.setRow(i, "", false);
+		}
+
+		view.setPageNumber(model.pageLabel());
+	}
+
+	/**
+	* Displays the details of the recipe the player clicked on the right page of the book.
+	*
+	* @param row the position of the line the player clicked
+	*/
+	public void openRecipe(int row) {
+		ArrayList<Recipe> page = model.recipesOnPage();
+
+		if(row < page.size())
+			view.setDetail(model.recipeDetail(page.get(row)));
+	}
+
+	/**
+	* Filters the spellbook using the text the player typed into the search bar.
+	*/
+	public void runSearch() {
+		model.search(view.getSearchText());
+		view.setDetail(model.recipeDetail(null));
+		writePage();
+	}
+
+	/**
+	* Moves to the next page of the spellbook.
+	*/
+	public void nextPage() {
+		model.nextPage();
+		writePage();
+	}
+
+	/**
+	* Moves back to the previous page of the spellbook.
+	*/
+	public void previousPage() {
+		model.previousPage();
+		writePage();
+	}
+
+	/**
+	* Closes the spellbook and returns the player to the home screen.
+	*/
 	public void back() {
 		this.main.HomeScreen(currentPlayer);
 	}
-	
-	//GETTER SETTERS
+
+	//Setters and getters
 	public SpellbookModel getModel() {
 		return model;
 	}
